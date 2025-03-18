aa1d4b91b8e13551212559990b2f8b278eeec0d9
SUSHMITHA V G <163226052+SUSHMITHA-V-G@users.noreply.github.com>
2025-03-18 15:07:43 +0530

    NEW : Project upload

---
+++ Project 2 - Face Recognition/src2.py
@@ -0,0 +1,50 @@
+import cv2,numpy , os
+haar = 'C:/Users/sen_s/Downloads/haarcascade_frontalcatface.xml'
+datasets ='Datasets'
+print('Training...')
+(images, labels , names , id)=([],[],{},0)
+for (subdirs , dirs , files ) in os.walk(datasets):
+	for subdir in dirs :
+		names[id] = subdir
+		sbjpath = os.path.join(datasets , subdir)
+		for filename in os.listdir(sbjpath):
+			path = sbjpath +'/'+filename
+			label = id
+			images.append(cv2.imread(path,0))
+			labels.append(int(label))
+		id+=1
+(width,height)=(130,100)
+(images,labels)=[numpy.array(lis) for lis in [images,labels]]
+model = cv2.face.FisherFaceRecognizer_create()
+model.train(images,labels)
+face_cascade = cv2.CascadeClassifier(haar)
+webcam = cv2.VideoCapture(0)
+cnt =0
+while True:
+	(_,im)=webcam.read()
+	gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3,5)
+
+	for (x,y,w,h) in faces:
+		face = gray[y:y+h , x:x+w]
+		face_resize = cv2.resize(face,(width, height))
+		prediction = model.predict(face_resize)
+		cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),3)
+		if prediction[1]<800:
+			cv2.putText(im,'%s - %.0f'%(names[prediction[0]],prediction[1]),(x-10, y-10),cv2.FONT_HERSHEY_COMPLEX,1,(51,255,255))
+			print(names[prediction[0]])
+			cnt =0
+		else:
+			cnt+=1
+			cv2.putText(im,'Unknown',(x-10,y-10),cv2.FONT_HERSHEY_PLAIN,1,(0,255,0))
+			if(cnt>100):
+				print("Unknown")
+				cv2.imwrite(r"C:\Users\sen_s\Downloads\OIP.jpg", im)
+
+				cnt =0
+	cv2.imshow('OpenCv',im)
+	key = cv2.waitKey(10)
+	if key == 27:
+		break
+webcam.release()
+cv2.destroyAllWindows()
